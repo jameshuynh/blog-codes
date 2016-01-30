9b977006201b608f805d82019c50945b71ffb27b
James Huynh <james@rubify.com>
2016-01-30 13:17:39 +0800

    fix style

--- ajax-pagination-demo/app/controllers/books_controller.rb
+++ ajax-pagination-demo/app/controllers/books_controller.rb
@@ -1,10 +1,8 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.page(params[:page] || 1).per(5)
-    if request.xhr?
-      render action: :index, layout: false
-    else
-      render action: :index
-    end
+    @books = Book.page(params[:page] || 1).per(4)
+    # ajax request will result in request.xhr? not nil 
+    # layout will be true if request is not an ajax request
+    render action: :index, layout: request.xhr? == nil 
   end
 end
